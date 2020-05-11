e651b1f888439f1489b29834d75554a0564b97d4
Louis <louis.fowler@gmail.com>
2020-05-11 16:15:43 +1200

    Updates

---
+++ precache-manifest.9ba609fd6897ca7e021508deb24e9531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d1c4392ba91ecfbef30cf18f8126a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2a4953cda18d183a7bd",
+    "url": "/static/css/main.f7de44b7.chunk.css"
+  },
+  {
+    "revision": "3bfcea48cf8afbfb3c53",
+    "url": "/static/js/2.9c5b48d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9c5b48d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a4953cda18d183a7bd",
+    "url": "/static/js/main.c38b48df.chunk.js"
+  },
+  {
+    "revision": "6d33eb7d3b3ac57630d5",
+    "url": "/static/js/runtime-main.c4e0f96d.js"
+  }
+]);
